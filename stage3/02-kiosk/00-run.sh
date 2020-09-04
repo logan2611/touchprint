61e706f397394d9e98c6b742380e3361b70a70f4
Logan Gartner <logan@qtechofficial.com>
2020-09-04 17:07:51 -0600

    Added default .xtimeout

--- stage3/02-kiosk/00-run.sh
+++ stage3/02-kiosk/00-run.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p "${ROOTFS_DIR}/home/kiosk"
 install -m 755 files/.xprofile "${ROOTFS_DIR}/home/kiosk/.xprofile"
 install -m 755 files/.browser.sh "${ROOTFS_DIR}/home/kiosk/.browser.sh"
+install -m 755 files/.xtimeout "${ROOTFS_DIR}/home/kiosk/.xtimeout"
 
 mkdir -p "${ROOTFS_DIR}/home/kiosk/.config/openbox"
 install -m 644 files/autostart "${ROOTFS_DIR}/home/kiosk/.config/openbox/autostart"
